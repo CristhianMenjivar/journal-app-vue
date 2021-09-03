befd11c9df85dd60db19d2c96fb6c885bfe93cfb
cristhian_menjivar <cristhian_menjivar@credicomer.com.sv>
2021-09-03 11:43:21 -0600

    interceptor en axios

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -4,4 +4,18 @@ const journalApi = axios.create({
   baseURL: "https://vue-demos-803df-default-rtdb.firebaseio.com",
 });
 
+// el interceptor se ejecutara en cada request
+journalApi.interceptors.request.use((config) => {
+  config.params = {
+    auth: localStorage.getItem("idToken"),
+  };
+
+  // si el backend pide header authentication
+  // config.headers = {
+  //   authorization: localStorage.getItem("idToken"),
+  // };
+
+  return config;
+});
+
 export default journalApi;
